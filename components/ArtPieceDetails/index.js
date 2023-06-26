6a25fa1267cd471988e953e02d73c712e8853b13
arnelakapllani <arnela.kapllani@web.de>
2023-06-26 12:44:07 +0200

    add BackButton

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <>
       <h2>Details</h2>
+      <button>
+        <Link href="/art-pieces">Go back</Link>
+      </button>
       <Image src={image} alt={title} width={200} height={200} />
       <h3>
         {title} ({year})
